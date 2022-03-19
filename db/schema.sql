9bd9c6ab35693b5ff85bf331ffdcfdbf7cf1ac7a
Ridercogswell <ridercogswell@gmail.com>
2022-03-19 15:17:09 -0700

    candidates can change party, if party exists

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,7 @@
+-- candidates must be dropped before parties becuase of the constraint relying on parties.id
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
+
 CREATE TABLE parties (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
@@ -8,5 +12,8 @@ CREATE TABLE candidates (
     id INTEGER auto_increment PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    industry_connected BOOLEAN NOT NULL
+    party_id INTEGER,
+    industry_connected BOOLEAN NOT NULL,
+    -- SETS FOREIGN KEY ***** MUST BE BELOW THE RELIED UPON TABLE (IN THIS CASE parties)
+    CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
